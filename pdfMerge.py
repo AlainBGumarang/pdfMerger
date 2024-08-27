907b199b33772476edf76bc021b8e3310578b0ca
kuyalainII <alaingumarangII@gmail.com>
2024-08-27 14:35:26 -0700

    update1
    Imported libraries

---
+++ pdfMerge.py
@@ -0,0 +1,9 @@
+# Alain B. Gumarang II
+# PDF Merger Project 
+# (based on "3 PYTHON AUTOMATION PROJECTS FOR BEGINNERS" by Internet Made Coder)
+# https://youtu.be/vEQ8CXFWLZU?si=GSU_BR4ldmIDsWEj
+
+import PyPDF2
+import sys
+import os
+
